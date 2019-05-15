afc4da03e9861c54df9112a7485fc3b8a28afde4
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2019-05-15 08:12:55 +0200

    Update SiteController.php
    SiteController osztály létrehozása.

--- Src/controllers/SiteController.php
+++ Src/controllers/SiteController.php
@@ -1,1 +1,17 @@
 
+<?php
+
+namespace app\controllers;
+
+use app\models\RegisterForm;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+}
