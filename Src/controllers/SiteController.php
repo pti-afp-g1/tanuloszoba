31886868d695b86f13813067a86c7f1eb071203f
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2019-05-15 08:23:48 +0200

    Update SiteController.php
    actionLogin függvény létrehozása

--- Src/controllers/SiteController.php
+++ Src/controllers/SiteController.php
@@ -55,4 +55,20 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
 }
