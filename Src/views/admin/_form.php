e7630cf1031724d82ca1904844801f4bf47ae342
bajzat.gifts@gmail.com <bajzat.gifts@gmail.com>
2019-05-15 15:38:22 +0200

    user-form class elkészítése

---
+++ Src/views/admin/_form.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true, 'autocomplete' => 'new-password', 'value' => '']) ?>
+
+    <?= $form->field($model, 'roleName')->dropDownList([
+        'admin' => Yii::t('app', 'admin'),
+        'teacher' => Yii::t('app', 'teacher')
+    ], ['prompt' => Yii::t('app', 'student')]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
