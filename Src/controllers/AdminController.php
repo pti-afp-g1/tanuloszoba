d649606cf3eb8efb80a4fd502e541f5644721437
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2019-05-15 08:52:44 +0200

    Update AdminController.php
    AdminController osztály létrehozása.

--- Src/controllers/AdminController.php
+++ Src/controllers/AdminController.php
@@ -1,1 +1,15 @@
 
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\User;
+use app\models\UserSearch;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+use yii\db\StaleObjectException;
+use \Throwable;
