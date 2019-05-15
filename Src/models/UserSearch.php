4d5a42b739233617a1e2277c28885eb4b8ebdb24
asanyad <asanya.d@gmail.com>
2019-05-15 19:13:22 +0200

    UserSearch - search metódus hozzáadva

--- Src/models/UserSearch.php
+++ Src/models/UserSearch.php
@@ -21,4 +21,49 @@ class UserSearch extends User {
     public function scenarios() {
         return Model::scenarios();
     }
+	
+    public function search($params) {
+        $query = User::find();
+
+        //keresési feltételeket itt adhatunk meg
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $dataProvider->setSort([
+            'attributes' => [
+                'username',
+                'email',
+                'roleName' => [
+                    'asc' => ['afp2_auth_assignment.item_name' => SORT_DESC],
+                    'desc' => ['afp2_auth_assignment.item_name' => SORT_ASC],
+                    'defaultSort' => 'asc',
+                    'label' => 'Szerep'
+                ]
+            ]
+        ]);
+
+        $this->load($params);
+
+        $query->joinWith('authAssignment');
+
+        //szűrési feltételeket itt adhatunk meg
+		
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ]);
+
+        $query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'password', $this->password]);
+
+        if (strtolower($this->roleName === 'student')) {
+            $query->andFilterWhere(['is', 'afp2_auth_assignment.item_name', new Expression('null')]);
+        } else {
+            $query->andFilterWhere(['like', 'afp2_auth_assignment.item_name', $this->roleName]);
+        }
+
+        return $dataProvider;
+    }
 }
