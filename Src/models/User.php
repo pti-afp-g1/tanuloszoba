9c92e58eefc12ba54ce98a698441fbbf4eb26fce
asanyad <asanya.d@gmail.com>
2019-05-15 18:36:06 +0200

    User osztály létrehozva

---
+++ Src/models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+use \Exception;
+
+class User extends ActiveRecord implements IdentityInterface {
+	
+}
