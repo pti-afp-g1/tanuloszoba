483fcd8ff1a020d6584ed045f2ad608efa5331d9
asanyad <asanya.d@gmail.com>
2019-05-15 04:01:39 +0200

    LoginForm - validatePassword metódus hozzáadva

--- Src/models/LoginForm.php
+++ Src/models/LoginForm.php
@@ -21,4 +21,15 @@ class LoginForm extends Model
             ['password', 'validatePassword'],
         ];
     }
+	
+    public function validatePassword($attribute, $params)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->getUser();
+
+            if (!$user || !$user->validatePassword($this->password)) {
+                $this->addError($attribute, 'Incorrect username or password.');
+            }
+        }
+    }
 }
