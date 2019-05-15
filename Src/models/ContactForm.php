a785ec508a330e792cdc034647e7e1bc7d3ca02d
asanyad <asanya.d@gmail.com>
2019-05-15 03:50:19 +0200

    ContactForm - contact metódus hozzáadva

--- Src/models/ContactForm.php
+++ Src/models/ContactForm.php
@@ -28,4 +28,20 @@ class ContactForm extends Model
             'verifyCode' => 'Verification Code',
         ];
     }
+	
+    public function contact($email)
+    {
+        if ($this->validate()) {
+            Yii::$app->mailer->compose()
+                ->setTo($email)
+                ->setFrom([Yii::$app->params['senderEmail'] => Yii::$app->params['senderName']])
+                ->setReplyTo([$this->email => $this->name])
+                ->setSubject($this->subject)
+                ->setTextBody($this->body)
+                ->send();
+
+            return true;
+        }
+        return false;
+    }
 }
