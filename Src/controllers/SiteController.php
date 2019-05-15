5e404ba3cce1a3105b0d5c6d89d871bcd439ca1c
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2019-05-15 08:30:44 +0200

    Update SiteController.php
    actionLogout függvény hozzáadása.

--- Src/controllers/SiteController.php
+++ Src/controllers/SiteController.php
@@ -89,4 +89,11 @@ class SiteController extends Controller
         ]);
     }
 
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+    
 }
