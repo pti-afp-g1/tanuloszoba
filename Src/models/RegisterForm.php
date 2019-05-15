e9c69a1e47e4b7df99542904961ba4dd69035073
asanyad <asanya.d@gmail.com>
2019-05-15 18:29:21 +0200

    RegisterForm - rules metódus hozzáadva

--- Src/models/RegisterForm.php
+++ Src/models/RegisterForm.php
@@ -10,5 +10,14 @@ class RegisterForm extends Model
     public $username;
     public $password;
     public $email;
-	
+		
+    public function rules()
+    {
+        return [
+            [['username', 'password', 'email'], 'required'],
+            ['username', 'validateUsername'],
+            ['email', 'validateEmail'],
+            ['email', 'email'],
+        ];
+    }
 }
