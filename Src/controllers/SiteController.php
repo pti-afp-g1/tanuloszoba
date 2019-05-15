112025560efbe3dbc5d121581c65c776eb0658c8
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2019-05-15 08:14:54 +0200

    Update SiteController.php
    behaviors függvény létrehozása

--- Src/controllers/SiteController.php
+++ Src/controllers/SiteController.php
@@ -14,4 +14,27 @@ use app\models\ContactForm;
 
 class SiteController extends Controller
 {
+      public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
 }
