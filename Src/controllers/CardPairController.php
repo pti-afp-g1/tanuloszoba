5e89e0a5ad8026aed65bf927374d9ad5568ed928
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2019-05-16 14:12:14 +0200

    Update CardPairController.php
    CardPairController osztály létrehozása.

--- Src/controllers/CardPairController.php
+++ Src/controllers/CardPairController.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace app\controllers;
+
+use thamtech\uuid\helpers\UuidHelper;
+use Throwable;
+use Yii;
+use app\models\CardPair;
+use app\models\CardPairSearch;
+use yii\db\StaleObjectException;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+
+class CardPairController extends Controller {
+
+}
