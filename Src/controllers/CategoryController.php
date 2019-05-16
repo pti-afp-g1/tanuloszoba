0e198dc746c52a07bf0d5a2674665f3529acf8a5
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2019-05-16 14:48:04 +0200

    Update CategoryController.php
    actionUpdate függvény létrehozása

--- Src/controllers/CategoryController.php
+++ Src/controllers/CategoryController.php
@@ -62,5 +62,18 @@ class CategoryController extends Controller
         return $this->render('create', [
             'model' => $model,
         ]);
-    }    
+    }
+    
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
 }
