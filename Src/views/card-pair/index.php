079390523feae87c87a4e8a8cd411e81d913409d
bajzat.gifts@gmail.com <bajzat.gifts@gmail.com>
2019-05-16 18:18:06 +0200

    card-pair-index class elkészítése

---
+++ Src/views/card-pair/index.php
@@ -0,0 +1,54 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\CardPairSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Card Pairs');
+?>
+<div class="card-pair-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Card Pair'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            [
+                'attribute' => 'card1',
+                'value' => function ($data) {
+                    return '/uploads/' . $data->card1;
+                },
+                'format' => ['image', ['width' => '125', 'height' => '175', 'class' => 'img-border']],
+            ],
+            [
+                'attribute' => 'card2',
+                'value' => function ($data) {
+                    return '/uploads/' . $data->card2;
+                },
+                'format' => ['image', ['width' => '125', 'height' => '175', 'class' => 'img-border']],
+            ],
+            [
+                'attribute' => 'Kategória',
+                'value' => function ($data) {
+                    return $data->category->title;
+                }
+            ],
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
