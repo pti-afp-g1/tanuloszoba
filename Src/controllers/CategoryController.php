4e39f2e4e31ca9661df1f1c4d0d2d0c25193ba32
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2019-05-16 14:35:36 +0200

    Create CategoryController.php
    CategoryController.php fájl létrehozása

---
+++ Src/controllers/CategoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Category;
+use app\models\CategorySearch;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
