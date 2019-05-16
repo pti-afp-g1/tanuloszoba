b43a001fe80ad00adbe46a42b9758fb1cdd7532a
asanyad <asanya.d@gmail.com>
2019-05-16 18:51:09 +0200

    CardPairSearch - rules metódus hozzáadva

--- Src/models/CardPairSearch.php
+++ Src/models/CardPairSearch.php
@@ -8,5 +8,11 @@ use app\models\CardPair;
 
 class CardPairSearch extends CardPair
 {
-	
+    public function rules()
+    {
+        return [
+            [['id', 'afp2_category_id', 'afp2_user_id'], 'integer'],
+            [['card1', 'card2'], 'safe'],
+        ];
+    }	
 }
