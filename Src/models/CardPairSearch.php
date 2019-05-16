e7973dcc398ad777e02545571cac7b44321074c7
asanyad <asanya.d@gmail.com>
2019-05-16 18:53:15 +0200

    CardPairSearch - search metódus hozzáadva

--- Src/models/CardPairSearch.php
+++ Src/models/CardPairSearch.php
@@ -20,4 +20,33 @@ class CardPairSearch extends CardPair
     {
         return Model::scenarios();
     }
+	
+    public function search($params)
+    {
+        $query = CardPair::find();
+
+        //feltételek helye
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            return $dataProvider;
+        }
+
+        //szűrő feltételek helye
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'afp2_category_id' => $this->afp2_category_id,
+            'afp2_user_id' => $this->afp2_user_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'card1', $this->card1])
+            ->andFilterWhere(['like', 'card2', $this->card2]);
+
+        return $dataProvider;
+    }
 }
