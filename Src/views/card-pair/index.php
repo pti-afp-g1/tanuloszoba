c07ad91de3226c13c5c5b77a54b14caa9c7ecc81
bajzat.gifts@gmail.com <bajzat.gifts@gmail.com>
2019-05-16 18:28:37 +0200

    Megjegyzések törlése, formázás

--- Src/views/card-pair/index.php
+++ Src/views/card-pair/index.php
@@ -3,9 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 
-/* @var $this yii\web\View */
-/* @var $searchModel app\models\CardPairSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
+
 
 $this->title = Yii::t('app', 'Card Pairs');
 ?>
