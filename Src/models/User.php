4661121b89d201e74cb790e8180b683378c2ba9f
Tibor Nagy <tibornagy77@gmail.com>
2019-05-16 10:44:15 +0200

    Hibajavítások a tesztek alapján

--- Src/models/User.php
+++ Src/models/User.php
@@ -2,20 +2,38 @@
 
 namespace app\models;
 
+use Exception;
 use Yii;
 use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
-use \Exception;
 
+/**
+ * This is the model class for table "{{%user}}".
+ *
+ * @property string $id
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ *
+ * @property CardPair[] $cardPairs
+ * @property Category[] $categories
+ * @property LexicalGame[] $lexicalGames
+ * @property MemoryGame[] $memoryGames
+ */
 class User extends ActiveRecord implements IdentityInterface {
-	
+
     private $_oldAttributes;
-	
+    /**
+     * {@inheritdoc}
+     */
     public static function tableName() {
         return 'afp2_user';
     }
-	
+
+    /**
+     * {@inheritdoc}
+     */
     public function rules() {
         return [
             [['username', 'email'], 'required'],
@@ -24,64 +42,139 @@ class User extends ActiveRecord implements IdentityInterface {
             [['username'], 'string', 'max' => 45],
             [['email'], 'string', 'max' => 99],
             [['password'], 'string', 'max' => 255],
+            [['username'], 'unique'],
+            [['email'], 'unique'],
         ];
     }
-	
+
+    /**
+     * {@inheritdoc}
+     */
     public function attributeLabels() {
         return [
             'id' => Yii::t('app', 'ID'),
             'username' => Yii::t('app', 'Username'),
             'email' => Yii::t('app', 'Email'),
             'password' => Yii::t('app', 'Password'),
-            'authAssignment' => 'Szerepkör',
-            'roleName' => 'Szerep'
+            'roleName' => Yii::t('app', 'Role Name'),
         ];
     }
-	
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getCardPairs() {
+        return $this->hasMany(CardPair::class, ['afp2_user_id' => 'id']);
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getCategories() {
+        return $this->hasMany(Category::class, ['afp2_user_id' => 'id']);
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getLexicalGames() {
+        return $this->hasMany(LexicalGame::class, ['afp2_user_id' => 'id']);
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getMemoryGames() {
+        return $this->hasMany(MemoryGame::class, ['afp2_user_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return UserQuery the active query used by this AR class.
+     */
     public static function find() {
         return new UserQuery(get_called_class());
     }
-	
+
+    /**
+     * @param int|string $id
+     * @return User|IdentityInterface|null
+     */
     public static function findIdentity($id) {
         return static::find()->where(['id' => $id])->one();
     }
-	
+
+    /**
+     * @param mixed $token
+     * @param null $type
+     * @return User|IdentityInterface|null
+     */
     public static function findIdentityByAccessToken($token, $type = null) {
         return static::find()->where(['id' => 1])->one();
     }
-	
+
+    /**
+     * @return int|string|void
+     */
     public function getId() {
         return $this->id;
     }
-	
+
+    /**
+     * @return string|void
+     */
     public function getAuthKey() {
-        //implementáljuk, ha szükségesséválik
+        // TODO: Implement getAuthKey() method.
     }
-	
+
+    /**
+     * @param string $authKey
+     * @return bool|void
+     */
     public function validateAuthKey($authKey) {
-        //implementáljuk, ha szükségesséválik
+        // TODO: Implement validateAuthKey() method.
     }
-	
+
+    /**
+     * @param $username
+     * @return User|null
+     */
     public static function findByUsername($username) {
         return static::find()->where(['username' => $username])->one();
     }
-	
+
+    /**
+     * @param $email
+     * @return User|array|null
+     */
     public static function findByEmail($email) {
         return static::find()->where(['email' => $email])->one();
     }
-	
+
+    /**
+     * @param $password
+     * @return bool
+     */
     public function validatePassword($password) {
         return password_verify($password, $this->password);
     }
-	
+
     public function afterFind() {
         $this->_oldAttributes = $this->attributes;
     }
-	
+
+    /**
+     * @param $attribute
+     * @return bool
+     */
     public function isChanged($attribute) {
         return $this->_oldAttributes[$attribute] !== $this->attributes[$attribute];
     }
-	
+
+    /**
+     * @param bool $insert
+     * @return bool
+     */
     public function beforeSave($insert) {
         if (!$insert && $this->isChanged('password')) {
             if (empty($this->password)) {
@@ -93,8 +186,7 @@ class User extends ActiveRecord implements IdentityInterface {
         return parent::beforeSave($insert);
     }
 
-    public function assignNewRole($role)
-    {
+    public function assignNewRole($role) {
         $this->revokeAllRole();
         $this->assignRole($role);
     }
@@ -109,16 +201,6 @@ class User extends ActiveRecord implements IdentityInterface {
         }
     }
 
-    private function revokeRole($role) {
-        $auth = Yii::$app->authManager;
-        $authorRole = $auth->getRole($role);
-        try {
-            return $auth->revoke($authorRole, $this->id);
-        } catch (Exception $e) {
-            return false;
-        }
-    }
-
     private function revokeAllRole() {
         $auth = Yii::$app->authManager;
         try {
@@ -127,15 +209,24 @@ class User extends ActiveRecord implements IdentityInterface {
             return false;
         }
     }
-	
+
+    /**
+     * @return ActiveQuery
+     */
     public function getAllAuthAssignment() {
         return $this->hasMany(AuthAssignment::class, ['user_id' => 'id']);
     }
-	
+
+    /**
+     * @return ActiveQuery
+     */
     public function getAuthAssignment() {
         return $this->hasOne(AuthAssignment::class, ['user_id' => 'id']);
     }
-	
+
+    /**
+     * @return string
+     */
     public function getRoleName() {
         return $this->authAssignment->item_name ?? 'student';
     }
