f508258005f84255a82b6512cdbc9ab1521752c5
bajzat.gifts@gmail.com <bajzat.gifts@gmail.com>
2019-05-16 18:26:35 +0200

    Megjegyzések törlése, formázás

--- Src/views/card-pair/_form.php
+++ Src/views/card-pair/_form.php
@@ -6,9 +6,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\CardPair */
-/* @var $form yii\widgets\ActiveForm */
+
 UploadAsset::register($this);
 ?>
 
