bab25be4b2332a48cf2595af71972c625a1b20d0
asanyad <asanya.d@gmail.com>
2019-05-16 18:42:29 +0200

    CardPair - rules metódus hozzáadva

--- Src/models/CardPair.php
+++ Src/models/CardPair.php
@@ -12,4 +12,15 @@ class CardPair extends ActiveRecord
 	{
         return '{{%card_pair}}';
     }
+	
+    public function rules() 
+	{
+        return [
+            [['card1', 'card2', 'afp2_category_id'], 'required'],
+            [['afp2_category_id', 'afp2_user_id'], 'integer'],
+            [['card1', 'card2'], 'string', 'max' => 255],
+            [['afp2_category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::class, 'targetAttribute' => ['afp2_category_id' => 'id']],
+            [['afp2_user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['afp2_user_id' => 'id']],
+        ];
+    }
 }
