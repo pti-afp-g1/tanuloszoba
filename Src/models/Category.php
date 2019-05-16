7941ec7e98467d5bc6370d9f6d01e0652cd2f48a
asanyad <asanya.d@gmail.com>
2019-05-16 19:01:39 +0200

    Category - attributeLabels metódus hozzáadva

--- Src/models/Category.php
+++ Src/models/Category.php
@@ -22,4 +22,13 @@ class Category extends ActiveRecord
             [['afp2_user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['afp2_user_id' => 'id']],
         ];
     }	
+	
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'title' => Yii::t('app', 'Title'),
+            'afp2_user_id' => Yii::t('app', 'Afp2 User ID'),
+        ];
+    }
 }
